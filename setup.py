3832bf1a0db7fcec98f3bb642968e2ad8eec6169
Nik <nikvaes@gmail.com>
2020-03-22 13:36:22 +0100

    feat(release): Release version 2.0.0

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup
+from os import path
 
 # read the contents of your README file
-from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+# setup library
 setup(
-    name='jiwer',
-    version='1.3.2',
-    description='Approximate the WER of an ASR transcript',
-    url='https://github.com/jitsi/asr-wer/',
-    author='Nik Vaessen',
-    author_email='nikvaes@gmail.com',
-    license='Apache 2',
-    packages=['jiwer'],
-    install_requires=["numpy"],
-    zip_safe=False,
+    name="jiwer",
+    version="2.0.0",
+    description="Approximate the WER of an ASR transcript",
+    url="https://github.com/jitsi/asr-wer/",
+    author="Nik Vaessen",
+    author_email="nikvaes@gmail.com",
+    license="Apache 2",
+    packages=["jiwer"],
+    setup_requires=["wheel", "twine"],
+    install_requires=["numpy", "python-Levenshtein"],
+    zip_safe=True,
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown",
+    test_suite="tests",
+    python_requires=">3",
 )
--- upload.sh
+++ upload.sh
@@ -1,4 +1,10 @@
+# remove compilation folder
 rm -r dist/
-python setup.py sdist bdist_wheel
+
+# this requrieres packages "wheel" and "twine"
+# install with pip
+python setup.py test sdist bdist_wheel
+
+# upload to pip
 # remove --repository-url argument to upload to real pipy
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
