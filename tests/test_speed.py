076c772b9ddb70b782e004250f8a05447008ba07
Nik Vaessen <nik@vaessen.tech>
2020-04-22 18:53:49 +0200

    fix speed benchmark, remove numpy requirements

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -1,38 +1,24 @@
-import time
-import numpy
-
 from jiwer import wer
 
 
-def execute_test(num_executions, num_sentences):
+def perform_computation(num_sentences):
     truth = ["this is a speed test" for _ in range(0, num_sentences)]
     hypo = ["this is not a speed test" for _ in range(0, num_sentences)]
 
-    times = []
-    for _ in range(num_executions):
-        start = time.monotonic()
-        wer(truth, hypo)
-        end = time.monotonic()
-
-        times.append(end - start)
-
-    times = numpy.array(times)
-    mean = numpy.mean(times)
-    var = numpy.std(times)
-
-    return mean, var
+    wer(truth, hypo)
 
 
-def test_with_n_sentences(num_sentences):
-    print("executing wer with {} sentences:".format(num_sentences))
-    mean, std = execute_test(10, num_sentences)
-    print("\tmean={:.4f} sec std={:.4f} sec".format(mean, std))
+def test_speed_n1(benchmark):
+    benchmark(perform_computation, 1)
 
 
-def main():
-    for n in [1, 10, 50, 100, 1000]:
-        test_with_n_sentences(n)
+def test_speed_n10(benchmark):
+    benchmark(perform_computation, 10)
 
 
-if __name__ == "__main__":
-    main()
+def test_speed_n100(benchmark):
+    benchmark(perform_computation, 100)
+
+
+def test_speed_n1000(benchmark):
+    benchmark(perform_computation, 1000)
